5d63a8188eddc303fe5fe16364357e0238c440ef
Bazyli Brzóska <bazyli.brzoska@gmail.com>
2022-08-17 15:51:39 -0700

    fix: eslint complaints

--- src/reactDomInterop.js
+++ src/reactDomInterop.js
@@ -1,3 +1,4 @@
+/* eslint-disable import/no-unresolved */
 import * as ReactDOM from 'external-react-dom'
 // eslint-disable-next-line import/no-default-export
 export default ReactDOM
--- src/reactInterop.js
+++ src/reactInterop.js
@@ -1,3 +1,4 @@
+/* eslint-disable import/no-unresolved */
 import * as React from 'external-react'
 // eslint-disable-next-line import/no-default-export
 export default React
