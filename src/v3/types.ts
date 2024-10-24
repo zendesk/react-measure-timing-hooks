290df84186bf6ec0dc0e0aabb57ff97cce873f0d
Evan Jenkins <evan.jenkins@zendesk.com>
2024-10-24 09:21:23 -0700

    feat(wip): partial implementation of createTraceRecording

--- src/v3/types.ts
+++ src/v3/types.ts
@@ -336,7 +336,6 @@ export interface TraceManagerConfig<ScopeT extends ScopeBase> {
 type MapTuple<KeysTuple extends readonly unknown[], MapToValue> = {
   [Index in keyof KeysTuple]: MapToValue // T[KeysTuple[Index]]
 }
-
 /**
  *
  */
@@ -416,7 +415,7 @@ export interface ComputedValueDefinition<
   matches: [...MatchersT]
   computeValueFromMatches: (
     // as many matches as match of type TraceEntry<ScopeT>
-    matches: MapTuple<MatchersT, TraceEntry<ScopeT>>,
+    matches: MapTuple<MatchersT, TraceEntryAndAnnotation<ScopeT>>,
   ) => number | string | boolean
 }
 
